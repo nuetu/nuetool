4d8dafa3b0a925d7b08f1c6bb5739cbc6b2f2302
bkeil <37550434+nuetu@users.noreply.github.com>
2022-12-01 13:00:35 -0500

    bot works
    launch using " node . " in terminal of server

--- app.js
+++ app.js
@@ -1,57 +1,51 @@
-// Require the necessary discord.js classes
-const fs = require("node:fs");
-const path = require("node:path");
-import { Client, Events, GatewayIntentBits } from "discord.js";
-import DISCORD_TOKEN from "dotenv";
+const {
+  Client,
+  GatewayIntentBits,
+  EmbedBuilder,
+  PermissionsBitField,
+  Permissions,
+} = require(`discord.js`);
+const { token } = require("./config.json");
+const prefix = "*";
+
+const list = {
+  lucas: "https://chuddy.dev",
+  ethan: "EthanElfGamer",
+  ben: "Your Captian",
+  help: "To use a command, type out an asterisk before one of the following words:\nhelp - displays command list, although you are already here (meta af)\nlucas - lucas stuff\nethan - alienelf-gainer?\nben - i made this bot, expect something stupid",
+};
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, (c) => {
-  console.log(`Ready! Logged in as ${c.user.tag}`);
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
 });
 
-// Log in to Discord with your client's token
-client.login(DISCORD_TOKEN);
+client.on("ready", () => {
+  console.log("Bot is moovin and groovin");
+  client.user.setActivity("Ready to buck some fitches up!", {
+    type: "PLAYING",
+  });
+});
 
-client.commands = new Collection(); //new command map
-const commandsPath = path.join(__dirname, "commands"); //open path to command directory
-const commandFiles = fs
-  .readdirSync(commandsPath)
-  .filter((file) => file.endsWith(".js")); //returns files, removes files that arent .js
+//functions when any message is sent
+client.on("messageCreate", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-for (const file of commandFiles) {
-  const filePath = path.join(commandsPath, file);
-  const command = require(filePath);
-  // Set a new item in the Collection with the key as the command name and the value as the exported module
-  if ("data" in command && "execute" in command) {
-    client.commands.set(command.data.name, command);
-  } else {
-    console.log(
-      `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-    );
-  }
-}
+  const args = message.content.slice(prefix.length).split(/ +/);
 
-client.on(Events.InteractionCreate, async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
+  const command = args.shift().toLowerCase();
 
-  const command = interaction.client.commands.get(interaction.commandName);
+  const messageArray = message.content.split(" ");
+  const argument = message.content.slice(1);
+  const cmd = messageArray[0];
 
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`);
-    return;
-  }
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({
-      content: "There was an error while executing this command!",
-      ephemeral: true,
-    });
+  if (command in list) {
+    message.channel.send(list[command]);
   }
 });
+
+client.login(token);
---
+++ commands.js
@@ -0,0 +1,6 @@
+const list = {
+  lucas: "https://chuddy.dev",
+  ethan: "EthanElfGamer",
+  ben: "Your Captian",
+  help: "To use a command, type out an asterisk before one of the following words:\nhelp\nlucas\nethan\nben",
+};
